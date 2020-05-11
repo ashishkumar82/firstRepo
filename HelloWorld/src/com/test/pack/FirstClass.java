6f6ec81080005fb195ed47bbcc5db91644ad94b9
ashish-at-164357337249 <ashish.kumar82@gmail.com>
2020-05-11 18:45:03 -0400

    First commit to local master

---
+++ HelloWorld/src/com/test/pack/FirstClass.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.test.pack;
+
+/**
+ * @author Ashish
+ *
+ */
+public class FirstClass {
+
+	/**
+	 * 
+	 */
+	public FirstClass() {
+		super();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
